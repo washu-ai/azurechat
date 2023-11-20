f55985a2491af74a9b6b14bc93f0efab6a965422
lackeyi <lackey_i@wustl.edu>
2023-11-20 12:27:04 -0600

    updated welcome text

--- src/features/theme/customise.ts
+++ src/features/theme/customise.ts
@@ -1,13 +1,10 @@
 export const AI_NAME = "Washington University ChatGPT Beta";
 export let HOME_MESSAGE = `
 Please note that, like the public instance of ChatGPT, this tool should not currently be used with data protected under HIPAA or FERPA, or any other sensitive information.
- 
 <br/><br/>
-The goal of creating this Beta instance of ChatGPT for the WashU community is to establish a secure sandbox environment for approved research and operational purposes. We are testing and working towards a security review by December 2024 to provide a fully secure and compliant tool.
-
+The goal of creating this Beta instance of ChatGPT for the WashU community is to establish a secure sandbox environment for approved research and operational purposes. We are testing and working towards a security review by December 2023 to provide a fully secure and compliant tool.
 <br/><br/>
-Please be aware that this Beta version has no content filters, such as for explicit words, and refrain from participating in any harmful or offensive activities. All user interactions are logged. Review the university's compliance policies for more information.  
-`;
+Please be aware that this Beta version has no content filters, such as for explicit words, and refrain from participating in any harmful or offensive activities. Review the university's <a href="https://wustl.edu/about/compliance-policies/" target="_blank">compliance policies</a> for more information. All user interactions are logged and linked to the user’s WUSTL key account.`;
 
 export const FEEDBACK_LINK = "https://wustl.az1.qualtrics.com/jfe/form/SV_afchOhQSyBJHxPw";
 export const PROVIDED_BY_TEXT = "Provided by WashU Digital Transformation and WashU IT";
