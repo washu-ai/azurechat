c52bcc35f011c42c9bdff1aa3799fc3007ceefc9
Eric Suiter <ericsuiter@gmail.com>
2024-06-18 09:40:43 -0500

    Update openai.ts

--- src/features/common/services/openai.ts
+++ src/features/common/services/openai.ts
@@ -3,9 +3,12 @@ import { OpenAI } from "openai";
 export const OpenAIInstance = () => {
   const openai = new OpenAI({
     apiKey: process.env.AZURE_OPENAI_API_KEY,
-    baseURL: `https://${process.env.AZURE_OPENAI_API_INSTANCE_NAME}.openai.azure.com/openai/deployments/${process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME}`,
+    baseURL: `https://${process.env.AZURE_OPENAI_API_ENDPOINT}/deployments/${process.env.AZURE_OPENAI_API_DEPLOYMENT_NAME}`,
     defaultQuery: { "api-version": process.env.AZURE_OPENAI_API_VERSION },
-    defaultHeaders: { "api-key": process.env.AZURE_OPENAI_API_KEY },
+    defaultHeaders: { 
+      "api-key": process.env.AZURE_OPENAI_API_KEY,
+      "Ocp-Apim-Subscription-Key": process.env.OCP_APIM_SUBSCRIPTION_KEY
+    },
   });
   return openai;
 };
@@ -23,9 +26,12 @@ export const OpenAIEmbeddingInstance = () => {
 
   const openai = new OpenAI({
     apiKey: process.env.AZURE_OPENAI_API_KEY,
-    baseURL: `https://${process.env.AZURE_OPENAI_API_INSTANCE_NAME}.openai.azure.com/openai/deployments/${process.env.AZURE_OPENAI_API_EMBEDDINGS_DEPLOYMENT_NAME}`,
+    baseURL: `https://${process.env.AZURE_OPENAI_API_ENDPOINT}/deployments/${process.env.AZURE_OPENAI_API_EMBEDDINGS_DEPLOYMENT_NAME}`,
     defaultQuery: { "api-version": process.env.AZURE_OPENAI_API_VERSION },
-    defaultHeaders: { "api-key": process.env.AZURE_OPENAI_API_KEY },
+    defaultHeaders: { 
+      "api-key": process.env.AZURE_OPENAI_API_KEY,
+      "Ocp-Apim-Subscription-Key": process.env.OCP_APIM_SUBSCRIPTION_KEY
+    },
   });
   return openai;
 };
