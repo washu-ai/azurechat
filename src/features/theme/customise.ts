125ec7e96bf3e1c56c06d29f832dba605145cf8a
Eric Suiter <ericsuiter@gmail.com>
2024-12-10 09:31:50 -0600

    Update customise.ts

--- src/features/theme/customise.ts
+++ src/features/theme/customise.ts
@@ -1,10 +1,10 @@
-export const AI_NAME = "Washington University ChatGPT";
+export const AI_NAME = "WashU ChatGPT";
 export let HOME_MESSAGE = `
 This fully secure sandbox is compliant for use with sensitive data, including information protected under HIPAA and FERPA.
 <br/><br/>
 Refrain from participating in harmful or offensive activities as per <a target="_blank" href="https://wustl.edu/about/compliance-policies/">university policies</a>.
 All interactions are logged and linked to the user's WUSTL Key account. 
-Learn more at <a href="https://genai.wustl.edu/chatgpt">genai.wustl.edu/chatgpt</a>.
+Learn more at <a target="_blank" href="https://genai.wustl.edu/chatgpt">genai.wustl.edu/chatgpt</a>.
 <br/><br/>
 Open a new prompt window by selecting the plus sign below.
 `;
