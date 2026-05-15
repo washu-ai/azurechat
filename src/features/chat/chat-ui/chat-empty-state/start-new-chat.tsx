17b602ddf27c2bd1cd153b7f45bbec5f60fd85f4
Eric Suiter <eric.suiter@wustl.edu>
2026-05-15 11:42:58 -0500

    Update start-new-chat.tsx

--- src/features/chat/chat-ui/chat-empty-state/start-new-chat.tsx
+++ src/features/chat/chat-ui/chat-empty-state/start-new-chat.tsx
@@ -12,7 +12,7 @@ export const StartNewChat: FC<Prop> = (props) => {
       <div className="col-span-2 gap-5 flex flex-col flex-1">
         <img src="/ai-icon.png" className="w-36" />
       </div>
-      <Card className="col-span-3 flex flex-col gap-5 p-5 bg-amber-50 border-amber-400 border-2">
+      <Card className="col-span-3 flex flex-col gap-5 p-5 bg-amber-50 border-amber-400 border-2 dark:bg-amber-900 dark:border-amber-500 dark:text-amber-50">
         <Typography variant="h4" className="text-primary">
           {AI_NAME}
         </Typography>
